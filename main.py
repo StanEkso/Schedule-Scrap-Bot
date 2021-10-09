4fbe5362ff2dad9092b1b49b06bc044f1151aca7
EksoVio <eksoxxx777@gmail.com>
2021-10-09 18:19:56 +0300

    master

--- main.py
+++ main.py
@@ -2,21 +2,52 @@ import telebot
 from telebot import types
 import sys
 import time
-import random
-import raspisanie
+import requests
+from bs4 import BeautifulSoup
+global msgs
 
 days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat','sun']
 call_days = {}
-msgs = []
-msgs.append(raspisanie.mon)
-msgs.append(raspisanie.tue)
-msgs.append(raspisanie.wed)
-msgs.append(raspisanie.thu)
-msgs.append(raspisanie.fri)
-msgs.append(raspisanie.sat)
-msgs.append(raspisanie.sun)
+
+def parsing():
+    global msgs
+    mon = "Расписание 02 группы на Понедельник: \n"
+    tue = "Расписание 02 группы на Вторник\n"
+    wed = "Расписание 02 группы на Среду\n"
+    thu = "Расписание 02 группы на Четверг\n"
+    fri = "Расписание 02 группы на Пятницу\n"
+    sat = "Расписание 02 группы на Субботу\n"
+    sun = 'Какие пары в Воскресенье? Поспи хоть, дружище...'
+
+
+
+    url = "https://mmf.bsu.by/ru/raspisanie-zanyatij/dnevnoe-otdelenie/1-kurs/2-gruppa/"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, features="html.parser")
+    time = soup.find_all('td', {'class': 'time'})
+    remarks = soup.find_all('td', {"class": "remarks"})
+    subjectteachers = soup.find_all('td', {"class": "subject-teachers"})
+    lecturepractice = soup.find_all('td', {'class': 'lecture-practice'})
+    room = soup.find_all('td', {'class':'room'})
+    weekday = soup.find_all('td', {'class':'weekday'})
+    for i in zip(time,remarks,subjectteachers,lecturepractice,room, weekday):
+        if i[5].text.lower() == "понедельник":
+            mon += i[0].text + " " + i[1].text + " " +i[2].text + " " + i[3].text + " " + i[4].text + '\n'
+        elif i[5].text.lower() == "вторник":
+            tue += i[0].text + " " + i[1].text + " " +i[2].text + " " + i[3].text + " " + i[4].text + '\n'
+        elif i[5].text.lower() == "среда":
+            wed += i[0].text + " " + i[1].text + " " + i[2].text + " " + i[3].text + " " + i[4].text + '\n'
+        elif i[5].text.lower() == "четверг":
+            thu += i[0].text + " " + i[1].text + " " +i[2].text + " " + i[3].text + " " + i[4].text + '\n'
+        elif i[5].text.lower() == "пятница":
+            fri += i[0].text + " " + i[1].text + " " + i[2].text + " " + i[3].text + " " + i[4].text + '\n'
+        elif i[5].text.lower() == "суббота":
+            sat += i[0].text + " " + i[1].text + " " + i[2].text + " " + i[3].text + " " + i[4].text + '\n'
+    msgs = [mon, tue, wed,thu,fri,sat,sun]
+    return msgs
 
 sended = 0
+parsing()
 
 reply_markup = types.InlineKeyboardMarkup()
 buttonPrev = types.InlineKeyboardButton(text="◀", callback_data='prev')
@@ -59,6 +90,7 @@ exampleFile.close()
 
 @bot.message_handler(commands=['расписание', 'schedule'])
 def table(message):
+    parsing()
     sended = 0
     schedule = types.InlineKeyboardMarkup()
     show = types.InlineKeyboardButton(text="Просмотреть", callback_data='show')
@@ -163,6 +195,7 @@ def mainteance(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
+    parsing()
     global sended
     global day
     day = call_days.get(call.message.id)
