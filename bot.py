cb44f43bfe32f84f689faa8a9596ae45123adf79
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-12-31 22:22:44 +0300

    fix: bootstraping

--- bot.py
+++ bot.py
@@ -1,14 +1,6 @@
 import os
-from aiogram import executor
-from telegram.main import dp, WEBAPP_HOST, on_startup, on_shutdown, WEBHOOK_PATH, WEBAPP_PORT, bootstrap_bot
-from api.main import app, bootstrap_api
-from shared.services.parsing import parser
+from telegram.main import bootstrap_bot
 
-
-import asyncio
-import threading
 
 if __name__ == '__main__':
-    apiThread = threading.Thread(target=bootstrap_api)
-    apiThread.start()
     bootstrap_bot()
