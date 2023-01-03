ee4769abf467e10592b362ea4d229ef6b991428d
StanEkso <90618842+StanEkso@users.noreply.github.com>
2023-01-03 20:28:01 +0300

    docs: for schedule service

--- telegram/services/schedule.py
+++ telegram/services/schedule.py
@@ -1,25 +1,31 @@
 from telegram.services.parsing import parser
 
-
-def formStringFromArray(arr: list) -> str:
-    return " ".join(arr)
+# Service for schedule.
 
 
 class ScheduleService:
+    # Current schedule.
     schedule: list[str]
 
+    # Generation of schedule via update method.
     def __init__(self) -> None:
         self.update()
         pass
 
+    # Method for updating schedule.
     def update(self) -> list[str]:
         self.schedule = parser.parseFromPage()
 
+    # Method for getting schedule.
     def get(self) -> list[str]:
         return self.schedule
 
+    # Method for getting schedule by day.
     def atDay(self, index: int) -> str:
-        return self.schedule[index]
+        try:
+            return self.schedule[index]
+        except:
+            return ""
 
 
 scheduleService = ScheduleService()
