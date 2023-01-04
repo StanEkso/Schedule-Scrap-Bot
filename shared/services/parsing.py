7311510b55b442ab83ca83a0c8a98c9c06c22d21
StanEkso <90618842+StanEkso@users.noreply.github.com>
2023-01-04 10:39:18 +0300

    feat: types for lesson, updated parser service

--- shared/services/parsing.py
+++ shared/services/parsing.py
@@ -2,11 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 import requests
 from shared.services.config import configService
-from telegram.services.message import messageService
+from shared.types.lesson import Lesson
 
-
-def formStringFromArray(arr: list) -> str:
-    return " ".join(arr)
+# Parser service for parsing schedule from site and return it as array of lessons
+# Lesson is a dict with keys: time, meta, subject, type, room, weekday
+# Example:
+# {
+#  "time": "8:30-10:00",
+#  "meta": "1-18",
+#  "subject": "Математика",
+#  "type": "Лекция",
+#  "room": "Ауд. 1",
+#  "weekday": "понедельник"
+# }
 
 
 class ParserService:
@@ -19,10 +27,15 @@ class ParserService:
     def getPageText(self) -> str:
         return requests.get(self.url).text
 
-    def parseFromPage(self):
+    def parseFromPage(self) -> list[Lesson]:
+        # Parsing content via BeautifulSoup.
         soup = BeautifulSoup(self.getPageText(), features="html.parser")
+
+        # Replacing <br> tags with spaces.
         for a in soup.findAll('br'):
             a.replaceWith(" %s " % a.text)
+
+        # Getting all needed tags.
         time = soup.find_all('td', {'class': 'time'})
         remarks = soup.find_all('td', {"class": "remarks"})
         subjectAndTeacher = soup.find_all('td', {"class": "subject-teachers"})
@@ -30,24 +43,35 @@ class ParserService:
         room = soup.find_all('td', {'class': 'room'})
         weekday = soup.find_all('td', {'class': 'weekday'})
 
-        lessons = []
+        lessons: list[Lesson] = []
+
+        # Zip all lists and iterate over them.
         for i in zip(time, remarks, subjectAndTeacher, lessonType, room, weekday):
 
-            type = i[3].text
-            currentWeekDay = i[5].text.lower()
-            if type == "л":
-                type = "Лекция"
-            elif type == "п":
-                type = "Практика"
-            else:
-                type = ""
+            # Getting data from table
+            currentTime = i[0].text
+            currentRemarks = i[1].text
+            currentSubjectAndTeacher = i[2].text
+            currentLessonType = i[3].text
+            currentRoom = i[4].text
 
-            lesson = {
-                "time": i[0].text,
-                "meta": i[1].text,
-                "subject": i[2].text,
-                "type": type,
-                "room": i[4].text,
+            # Using lower() method for getting day in lower case.
+            # Used for getting day from dictionary.
+            currentWeekDay = i[5].text.lower()
+
+            # Replacing short names of lesson types with full names.
+            if currentLessonType == "л":
+                currentLessonType = "Лекция"
+            elif currentLessonType == "п":
+                currentLessonType = "Практика"
+            else:
+                currentLessonType = ""
+            lesson: Lesson = {
+                "time": currentTime,
+                "meta": currentRemarks,
+                "subject": currentSubjectAndTeacher,
+                "type": currentLessonType,
+                "room": currentRoom,
                 "weekday": currentWeekDay
             }
             lessons.append(lesson)
@@ -55,4 +79,5 @@ class ParserService:
         return lessons
 
 
+# Create a parser service instance.
 parser: ParserService = ParserService()
---
+++ shared/types/lesson.py
@@ -0,0 +1,20 @@
+# Write a lesson class inheriting from TypedDict and use it in the ParserService class:
+
+from typing import TypedDict
+
+
+class Lesson(TypedDict):
+    time: str
+    meta: str
+    subject: str
+    type: str
+    room: str
+    weekday: str
+
+# Create a function taking a Lesson and returning a string
+# Example:
+# 8:15 1-18 Математика Лекция Ауд. 1
+
+
+def lessonToString(lesson: Lesson) -> str:
+    return f"{lesson['time']} {lesson['meta']} {lesson['subject']} {lesson['type']} {lesson['room']}"
