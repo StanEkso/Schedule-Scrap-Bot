a2daa3ab71734852a0eb4a82a4cb84955e9ca773
StanEkso <90618842+StanEkso@users.noreply.github.com>
2023-01-09 22:20:04 +0300

    refactor: remove useless code

--- telegram/structures/hash.py
+++ telegram/structures/hash.py
@@ -1,28 +1,3 @@
-from typing import Generic, TypeVar
-
-T = TypeVar("T")
-K = TypeVar("K")
-
-
-class Hash(Generic[T, K]):
-    table: dict[T, K]
-
-    def __init__(self) -> None:
-        super().__init__()
-        self = self
-        pass
-
-    def set(self, key: T, value: K) -> bool:
-        try:
-            self.table[key] = value
-            return True
-        except:
-            return False
-
-    def get(self, key: T) -> K:
-        return self.table.get(key)
-
-
 class IntegerHash:
     table: dict[str, int] = dict()
 
@@ -38,3 +13,6 @@ class IntegerHash:
 
     def get(self, key: str) -> int:
         return self.table.get(key)
+
+    def has(self, key: str) -> bool:
+        return key in self.table
