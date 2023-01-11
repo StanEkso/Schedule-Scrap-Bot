2084b275210e06b5a2a166102604346e3a1c1ebb
StanEkso <90618842+StanEkso@users.noreply.github.com>
2023-01-11 17:03:09 +0300

    fix: body of response

--- web/api.py
+++ web/api.py
@@ -1,5 +1,6 @@
 from aiohttp import web
 from shared.services.parsing import parser
+import json
 routes = web.RouteTableDef()
 
 
@@ -7,7 +8,8 @@ routes = web.RouteTableDef()
 async def endpoint(request: web.Request):
     return web.Response(text="Hello, world")
 
+
 @routes.get("/schedule")
 async def schedule(request: web.Request):
-    scheduleObj = parser.parseFromPage();
-    return web.Response(body=scheduleObj, content_type="application/json")
+    scheduleObj = parser.parseFromPage()
+    return web.Response(body=json.dump(scheduleObj), content_type="application/json")
